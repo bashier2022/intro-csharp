24457341fa57c3bd393259bb43009ac2641f1029
Bashier Watted <bwatted@yahoo.com>
2022-10-15 10:34:44 +0300

    comitt all

---
+++ lesson-16/DataAccess/DataBaseTablesDB.cs
@@ -0,0 +1,11 @@
+﻿using DbModel;
+using Microsoft.EntityFrameworkCore;
+namespace DataAccess
+{
+    public class DataBaseTablesDB:DbContext
+    {
+        public DbSet<Person> Students { get; set; }
+        
+        public 
+    }
+}
---
+++ lesson-16/DbModel/Model.cs
@@ -0,0 +1,27 @@
+﻿namespace DbModel
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Birthyear { get; set; }
+        public int Email { get; set; }
+
+        public List<Course> Courses { get; set; }
+    }
+
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+    }
+    public class AbsolvatedCourses
+    {
+        public int PersonId { get; set; }
+        public int CourseId { get; set; }
+        public int Mark { get; set; }
+    }
+    
+}
